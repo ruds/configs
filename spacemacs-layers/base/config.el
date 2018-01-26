2cc5754cb1b179f032e5021387f46f166b8a2c92
Matt Rudary <mattrudary@google.com>
2018-01-26 16:24:59 -0500

    Add Spacemacs Layers

---
+++ spacemacs-layers/base/config.el
@@ -0,0 +1,17 @@
+(defgroup base-layer nil "Customization for the base layer" :group 'local)
+
+(defcustom base/compile-func 'compile
+  "The function used to compile stuff."
+  :group 'base-layer
+  :type 'function)
+
+(defvar base/format-region-alist
+  '((c++-mode . clang-format-region)
+    (c-mode . clang-format-region)))
+
+(defvar base/format-buffer-alist
+  '((c++-mode . clang-format-buffer)
+    (c-mode . clang-format-buffer)
+    (go-mode . go-fmt)))
+
+(add-to-list 'auto-mode-alist '("patch$" . diff-mode))
